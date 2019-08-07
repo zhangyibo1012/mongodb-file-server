0c472fd1b6e6266e3edffc146b3e04bc8b189f56
zhangyibo1012 <35024391+zhangyibo1012@users.noreply.github.com>
2019-08-07 19:36:31 +0800

    '初始化项目'

---
+++ src/main/java/cn/orgtec/file/MongodbFileServerApplication.java
@@ -0,0 +1,17 @@
+package cn.orgtec.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 图片服务器
+ *
+ * @author Yibo Zhang
+ * @date 2019/08/07
+ */
+@SpringBootApplication
+public class MongodbFileServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbFileServerApplication.class, args);
+    }
+}
