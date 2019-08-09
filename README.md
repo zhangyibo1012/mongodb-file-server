91926ca689cfaf0a01e6ca7d8ab79d964d0079ba
zhangyibo1012 <35024391+zhangyibo1012@users.noreply.github.com>
2019-08-09 14:07:48 +0800

    '设置浏览器跨域配置'

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ mongodb-file-server
 ### 说明
 
 ```
-服务器部署 mongodb 3.4.9 设置用户名密码
+服务器部署 mongodb 3.4.9 设置用户名密码。
+更改 yml 配置文件 monodb 的 uri 和 address 的地址
 
 ```
 
---
+++ src/main/java/cn/orgtec/file/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package cn.orgtec.file.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 允许跨域请求
+ *
+ * @author Yibo Zhang
+ * @date 2019/08/07
+ */
+
+@Configuration
+@EnableWebMvc
+public class SecurityConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*") ;
+	}
+}
